0874e76eb402806793528e04541ccced510cde18
ibn2085 <yahyahridwan665@gmail.com>
2021-12-22 18:06:59 +0545

    making some commit

--- src/component/post/post-detail.js
+++ src/component/post/post-detail.js
@@ -2,21 +2,21 @@ export function PostDetail({data}){
     return(
         <div className="container" >
                 <div>
+                    <h1 className='font-2 font-md'>Name : {data.title}</h1>
+                    <div className="mt-1 mb-1" style={{width:'100%',height:'250px'}}>
+                        <img src={data.image} alt="crypto-details hub" style={{width:'100%',height:'100%',objectFit:'contain',borderRadius:'10px'}}/>
+                    </div>
                     <div className="mt-2">
-                        <h1 className='font-2 font-md'>Name : {data.title}</h1>
-                        <p className="text-gray">Author : {data.author}</p>
-                        <p className="text-black">Website : <a href={`https://${data.website}`} className="text-blue" target="_blank" rel="noreferrer">{data.website}</a></p>
-                        <p className="text-black">Supply : {data.supply}</p>
-                        <p className="text-gray">Chain-network : {data.chain}</p>
-                        <p className="text-gray">Contract : <a href={`https://www.google.com/search?q=${data.contract}`} target="_blank" rel="noreferrer">{data.contract}</a></p>
-                        <p className="text-gray" style={{float:'right'}}>Upload On: {new Date(data.createdAt).toDateString()}</p>
+                        <p className="text-black">Website: <a href={`https://${data.website}`} className="text-blue" target="_blank" rel="noreferrer">{data.website}</a></p>
+                        <p className="text-black">Supply: {data.supply}</p>
+                        <p className="text-gray">Contract: <a href={`https://www.google.com/search?q=${data.contract}`} target="_blank" rel="noreferrer">{data.contract}</a></p>
+                        <p className="text-gray">Chain-network: {data.chain}</p>
                     </div>
                     <div>
-                        <div className="mt-2 mb-2" style={{width:'100%',height:'250px'}}>
-                            <img src={data.image} alt="crypto-details hub" style={{width:'100%',height:'100%',objectFit:'contain',borderRadius:'10px'}}/>
-                        </div>
                         <div>
                             <p style={{textAlign:'justify'}}>{data.content}</p>
+                        <p className="text-gray" style={{float:'right'}}>Upload On: {new Date(data.createdAt).toDateString()}</p>
+                            <p className="text-gray">Author: {data.author}</p>
                         </div>
                     </div>
                 </div>
